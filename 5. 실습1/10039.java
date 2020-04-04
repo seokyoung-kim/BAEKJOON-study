973e13a08d8fd7ac9d3e527a10210573ef4c858e
seokyoung-kim <seokyoungkimm@gmail.com>
2020-04-04 23:38:12 +0900

    주석 첨가

--- 5. 실습1/10039.java
+++ 5. 실습1/10039.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
 
+
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
+		// 배열에 넣는 방법
 		int[] a = new int[5];
-
+		
 		for (int i = 0; i < 5; i++) {
 			a[i] = sc.nextInt();
 
