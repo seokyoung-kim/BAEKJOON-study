5fefb3ffb9ee6a7c3dfc47137d779572ce7fdfa6
seokyoung-kim <seokyoungkimm@gmail.com>
2020-04-06 09:34:11 +0900

    주석 수정

--- 6. 1차원 배열/10818.java
+++ 6. 1차원 배열/10818.java
@@ -13,7 +13,7 @@ public class Main {
 			a[i] = sc.nextInt();
 		}
 
-		// 최솟값
+		// 최솟값 계산
 		int max = a[0];
 		for (int i = 0; i < n; i++) {
 			if (a[i] > max) {
@@ -21,7 +21,7 @@ public class Main {
 			}
 		}
 
-		// 최댓값
+		// 최댓값 계산
 		int min = a[0];
 		for (int i = 0; i < n; i++) {
 			if (a[i] < min) {
